202340363fdb7aab50c03d50f351590f07519115
MrAlaz10 <103569969+MrAlaz10@users.noreply.github.com>
2026-04-14 01:37:13 -0500

    Update storage_searchers.py
    -fixed continue button logic

--- storage_searchers.py
+++ storage_searchers.py
@@ -1,9 +1,9 @@
 import tkinter as tk
 import random
 
-#------SAVED DATA-------
+# ------SAVED DATA-------
 inventory = {}
-filament = 100      #measured in grams
+filament = 100  # measured in grams
 money = 500
 items_crafted = 0
 items_sold = 0
@@ -13,15 +13,16 @@ old_3d_printer = False
 tutorial_completed = True
 current_state = ""
 purchased_units = []
-#---------LOOT--------------
-junk = ["broken vase","family scrapbook","old clothes","useless documents"," damaged sports equipment", "rusty tools"]
+# ---------LOOT--------------
+junk = ["broken vase", "family scrapbook", "old clothes", "useless documents", " damaged sports equipment",
+        "rusty tools"]
 box_items_common = [
-    "small battery", "cables","power supply","small speaker",
+    "small battery", "cables", "power supply", "small speaker",
     "circuit board", "large speaker", "medium battery", "computer fan"]
 box_items_rare = [
     "graphics card", "ram", "processor", "hard drive", "monitor",
     "solid state drive", "motherboard", "mechanical keyboard"
-    ]
+]
 box_items_legendary = [
     "vr headset",
     "vintage game console",
@@ -60,70 +61,76 @@ valuables_price = {
     "desktop computer": 1000,
     "laptop": 1200
 }
-filament_cost = {"desktop computer":500,"bluetooth speaker":150,"bluetooth headphones":50,"laptop":350}
-#--------------OTHER-----------------
+filament_cost = {"desktop computer": 500, "bluetooth speaker": 150, "bluetooth headphones": 50, "laptop": 350}
+# --------------OTHER-----------------
+box_amount = 0
 temp_boxes_opened = 0
 active_boxes = {}
-unit_prices = [150,300,500,600,800,1000]
-unit_price_multiplier = [2.0,2.50,3]
-box_colors = {1 : "burlywood", 2 : "peru", 3 : "tan", 4 : "saddlebrown"}
+unit_prices = [150, 300, 500, 600, 800, 1000]
+unit_price_multiplier = [2.0, 2.50, 3]
+box_colors = {1: "burlywood", 2: "peru", 3: "tan", 4: "saddlebrown"}
+font_colors = ["#6CF527","#27D3F5","#F54927"]
 
 
-def write(screen,text,newline=True,tag=None):
+def write(screen, text, newline=True, tag=None, color="white"):
     screen.configure(state="normal")
     if newline:
         text += "\n"
-    screen.insert(tk.END,text,tag)
+    screen.insert(tk.END, text, tag)
+    screen.configure(foreground=color)
     screen.configure(state="disabled")
 
 
-def buy_unit(index,price,clicked_button):
+def buy_unit(index, price, clicked_button):
     global money
     if money >= price:
         money -= price
         hud.config(text=f"${money}")
-        clicked_button.config(state="disabled",command=open_unit(), text=f"Unit #{index+1}\nPURCHASED")
-        purchased_units.append(f"Unit{index+1}")
+        clicked_button.config(state="disabled", command=open_unit(), text=f"Unit #{index + 1}\nPURCHASED")
+        purchased_units.append(f"Unit{index + 1}")
     else:
-        error_label.place(rely=0.5,relx=0.5,anchor="center")
+        error_label.place(rely=0.5, relx=0.5, anchor="center")
         error_label.lift()
-        root.after(2000,error_message_remove)
-        
+        root.after(2000, error_message_remove)
+
+
 def error_message_remove():
     error_label.place_forget()
-    
+
+
 def open_shop():
     pass
 
 
 def open_unit():
-    global boxes_opened, current_state, temp_boxes_opened
+    global boxes_opened, current_state, temp_boxes_opened, box_amount
     temp_boxes_opened = 0
-    box_amount = random.randint(5,8)
-    current_state = "opened_unit"
+    box_amount = random.randint(5, 8)
+    current_state = "open unit"
     background_screen.config(state="normal")
     unit_frame.place_forget()
-    
+
     background_screen.delete("1.0", tk.END)
     background_screen.config(bg="dimgray")
     hud.place(rely=0.95, relx=0.5, anchor="center")
+    write(background_screen, f"\n\n\nStorage Unit", True, "center")
     hud.lift()
     spawn_boxes(box_amount)
     if temp_boxes_opened == box_amount:
         continue_button.config(command=play_game)
-        continue_button.place(relx=0.1,rely=0.9,width=150,height=50)
-    
-    
+        continue_button.place(relx=0.1, rely=0.9, width=150, height=50)
+
+
 def play_game():
-    global current_state
+    global current_state, purchased_units
     current_state = "main lot"
     background_screen.configure(state="normal")
     background_screen.configure(background="gray")
     background_screen.delete("1.0", tk.END)
     continue_button.place_forget()
-    inventory_button.place(relx=0.7,rely=0.9,width=150,height=50)
+    inventory_button.place(relx=0.7, rely=0.9, width=150, height=50)
 
-    #hud.place(rely=0.95,relx=0.5,anchor="center")
+    # hud.place(rely=0.95,relx=0.5,anchor="center")
 
     # enumerate() gives us BOTH the position (index: 0, 1, 2...) AND the value (price: 150, 300...)
     for index, price in enumerate(unit_prices):
@@ -147,14 +154,18 @@ def play_game():
     unit_frame.place(relx=0.5, rely=0.5, anchor="center")
     write(background_screen, f"\n\n\nStorage Lot", True, "center")
 
+
 def intro_screen():
     background_screen.configure(state="normal")
-    background_screen.delete("1.0",tk.END)
+    background_screen.delete("1.0", tk.END)
     play_button.place_forget()
     quit_button.place_forget()
     settings_button.place_forget()
-    continue_button.place(relx=0.5,rely=0.9,anchor="center",width=150,height=50)
-    write(background_screen, f"\n\n\n\nWelcome to Storage Searchers, you've just lost \nyour job and taken everything out of your bank \naccount in order to fulfill a lifelong \ndream. Become rich. Buy a storage unit and pray \nto your god there's something valuable in it.", True, "center")
+    continue_button.place(relx=0.5, rely=0.9, anchor="center", width=150, height=50)
+    write(background_screen,
+          f"\n\n\n\nWelcome to Storage Searchers, you've just lost \nyour job and taken everything out of your bank \naccount in order to fulfill a lifelong \ndream. Become rich. Buy a storage unit and pray \nto your god there's something valuable in it.",
+          True, "center")
+
 
 def tutorial_unit():
     global boxes_opened, current_state
@@ -164,14 +175,15 @@ def tutorial_unit():
     background_screen.config(bg="dimgray")
     hud.place(rely=0.95, relx=0.5, anchor="center")
     continue_button.place_forget()
-    inventory_button.place(relx=0.7,rely=0.9,width=150,height=50)
+    inventory_button.place(relx=0.7, rely=0.9, width=150, height=50)
     write(background_screen, "\nGrandpa's Storage", True, "center")
     spawn_boxes(6)
+
 
 def generate_loot(amount_to_roll):
     rolled_items = []
     for index in range(amount_to_roll):
-        percentage = random.uniform(0.0,1.0)
+        percentage = random.uniform(0.0, 1.0)
         if percentage < 0.6:
             rolled_items.append(random.choice(junk))
         elif percentage < 0.9:
@@ -182,8 +194,9 @@ def generate_loot(amount_to_roll):
             rolled_items.append(random.choice(box_items_legendary))
     return rolled_items
 
+
 def open_box(button):
-    global boxes_opened, filament, old_3d_printer, active_boxes, temp_boxes_opened
+    global boxes_opened, filament, old_3d_printer, active_boxes, temp_boxes_opened,current_state,box_amount, font_colors
     temp_boxes_opened += 1
     boxes_opened += 1
     button.place_forget()
@@ -203,35 +216,46 @@ def open_box(button):
             else:
                 inventory[item] = 1
     for item in loot:
-        write(background_screen, f"\n{item}", True, "center")
-    if boxes_opened == 6:
-        continue_button.config(command=play_game)
-        continue_button.place(relx=0.1,rely=0.9,width=150,height=50)
-        active_boxes = {}
+        write(background_screen, item, True, "center")
+    if current_state == "tutorial":
+        if boxes_opened == 6:
+            continue_button.config(command=play_game)
+            continue_button.place(relx=0.1, rely=0.9, width=150, height=50)
+            active_boxes = {}
+    elif current_state == "open unit":
+        if temp_boxes_opened == box_amount:
+            continue_button.config(command=play_game)
+            continue_button.place(relx=0.1, rely=0.9, width=150, height=50)
+
+
 
 def show_inventory():
-    global boxes_opened, active_boxes
+    global boxes_opened, active_boxes, filament
     continue_button.place_forget()
-    if current_state != "main lot":
+    if current_state != "main lot" and current_state == "tutorial":
         for box in active_boxes:
             box.place_forget()
     elif current_state == "main lot":
         unit_frame.place_forget()
+    elif current_state == "open unit":
+        for box in active_boxes:
+            box.place_forget()
     background_screen.config(state="normal")
     background_screen.delete("1.0", tk.END)
     background_screen.config(bg="black")
-    inventory_back_button.place(relx=0.7,rely=0.9,width=150,height=50)
-    write(background_screen, f"\n\n{inventory}\n\n\n{boxes_opened}", True, "center")
+    inventory_back_button.place(relx=0.7, rely=0.9, width=150, height=50)
+    write(background_screen, f"\n\nInventory:\n{inventory}\n\n\n{boxes_opened}\n\n\n\nFilament: {filament}", True, "center")
+
 
 def return_from_inventory():
-    global active_boxes, current_state
+    global active_boxes, current_state, temp_boxes_opened, box_amount
     if current_state == "tutorial":
         background_screen.config(state="normal")
         background_screen.delete("1.0", tk.END)
         background_screen.config(bg="dimgray")
         hud.place(rely=0.95, relx=0.5, anchor="center")
         inventory_back_button.place_forget()
-        inventory_button.place(relx=0.7,rely=0.9,width=150,height=50)
+        inventory_button.place(relx=0.7, rely=0.9, width=150, height=50)
         if boxes_opened == 6:
             continue_button.config(command=play_game)
             continue_button.place(relx=0.1, rely=0.9, width=150, height=50)
@@ -249,16 +273,18 @@ def return_from_inventory():
         background_screen.config(state="normal")
         background_screen.delete("1.0", tk.END)
         background_screen.config(bg="dimgray")
-        ############################hud.place(rely=0.95, relx=0.5, anchor="center")
         inventory_back_button.place_forget()
-        inventory_button.place(relx=0.7,rely=0.9,width=150,height=50)
+        if temp_boxes_opened == box_amount:
+            continue_button.config(command=play_game)
+            continue_button.place(relx=0.1, rely=0.9, width=150, height=50)
+        inventory_button.place(relx=0.7, rely=0.9, width=150, height=50)
     for box in active_boxes:
         box.place(relx=active_boxes[box][0], rely=active_boxes[box][1])
 
 
 def spawn_boxes(max_amount):
     global box_colors, active_boxes
-    safe_zones = [(0.2, 0.2), (0.5, 0.3), (0.8, 0.2), (0.2,0.6),(0.5, 0.8),(0.8, 0.5),(0.4,0.5),(0.8,0.8)]
+    safe_zones = [(0.2, 0.2), (0.5, 0.3), (0.8, 0.2), (0.2, 0.6), (0.5, 0.8), (0.8, 0.5), (0.4, 0.5), (0.8, 0.8)]
     for index in range(max_amount):
         base_x = safe_zones[index][0]
         base_y = safe_zones[index][1]
@@ -269,50 +295,52 @@ def spawn_boxes(max_amount):
         box_color_picker = random.randint(1, 4)
         box_button = tk.Button(root, text="Open", bg=box_colors[box_color_picker], fg="white", font=("Fixedsys", 14),
                                width=random_width, height=random_height)
-        box_button.config(command= lambda b=box_button: open_box(b))
+        box_button.config(command=lambda b=box_button: open_box(b))
         box_button.place(relx=random_location_x, rely=random_location_y)
         active_boxes[box_button] = (random_location_x, random_location_y)
     hud.lift()
 
+
 def quit_game():
     root.destroy()
 
+
 def settings():
     background_screen.configure(state="normal")
-    background_screen.delete("1.0",tk.END)
+    background_screen.delete("1.0", tk.END)
     play_button.place_forget()
     quit_button.place_forget()
     settings_button.place_forget()
     write(background_screen, "\nSettings", True, "center")
 
 
-root =  tk.Tk()
+root = tk.Tk()
 root.title("Storage Searcher")
 root.geometry("800x700")
 root.configure(background="black")
 
 background_screen = tk.Text(root, padx=5, pady=5, background="black", foreground="white", font=("Fixedsys", 18))
-background_screen.pack(side="top", fill="x",expand=True)
+background_screen.pack(side="top", fill="x", expand=True)
 write(background_screen, "\n\n\n\n Storage Searchers", True, "center")
 
-hud = tk.Label(root,bg="gray",fg="white",text=f"${money}",font=("Fixedsys",16))
-inventory_label = tk.Label(root,bg="black",fg="white",font=("Fixedsys",16))
-error_label = tk.Label(root,bg="black",fg="red",text="ERROR",font=("Fixedsys",20))
+hud = tk.Label(root, bg="gray", fg="white", text=f"${money}", font=("Fixedsys", 16))
+inventory_label = tk.Label(root, bg="black", fg="white", font=("Fixedsys", 16))
+error_label = tk.Label(root, bg="black", fg="red", text="ERROR", font=("Fixedsys", 20))
 
-play_button = tk.Button(root, text="Play", command=intro_screen,background="white",foreground="black")
-quit_button = tk.Button(root, text="Quit", command=quit_game,background="white",foreground="black")
-settings_button = tk.Button(root, text="Settings", command=settings,background="white",foreground="black")
-continue_button = tk.Button(root, text="Continue", command=tutorial_unit,background="white",foreground="black")
-inventory_button = tk.Button(root, text="Inventory", command=show_inventory,background="white",foreground="black")
-inventory_back_button = tk.Button(root, text="Back", command=return_from_inventory,background="white",foreground="black")
+play_button = tk.Button(root, text="Play", command=intro_screen, background="white", foreground="black")
+quit_button = tk.Button(root, text="Quit", command=quit_game, background="white", foreground="black")
+settings_button = tk.Button(root, text="Settings", command=settings, background="white", foreground="black")
+continue_button = tk.Button(root, text="Continue", command=tutorial_unit, background="white", foreground="black")
+inventory_button = tk.Button(root, text="Inventory", command=show_inventory, background="white", foreground="black")
+inventory_back_button = tk.Button(root, text="Back", command=return_from_inventory, background="white",
+                                  foreground="black")
 
 # Create the invisible "cardboard box" to hold our buttons for main lot
 unit_frame = tk.Frame(root, bg="gray")
 
-play_button.place(relx=0.5,rely=0.5,anchor="center", width=150,height=50)
-quit_button.place(relx=0.5,rely=0.7,anchor="center",width=150,height=50)
-settings_button.place(relx=0.5,rely=0.6,anchor="center",width=150,height=50)
-
+play_button.place(relx=0.5, rely=0.5, anchor="center", width=150, height=50)
+quit_button.place(relx=0.5, rely=0.7, anchor="center", width=150, height=50)
+settings_button.place(relx=0.5, rely=0.6, anchor="center", width=150, height=50)
 
 background_screen.tag_configure("center", justify="center")
 
